0a6dc6c77e4220aa05d26b2046c68a8efb2958be
gwebb17 <gwebb1723@gmail.com>
2022-06-30 13:10:15 -0700

    wrote function plethora

---
+++ Basic Javascript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,17 @@
+function thisFunction() {
+    var C = 3*8;
+    document.getElementById("Math").innerHTML="3 times 8 = " + C
+}
+
+function subtraction_Function() {
+    var Subtraction = 5-2;
+    document.getElementById("Math").innerHTML= "5 -2 = " + Subtraction;
+}
+function division_Function() {
+    var Division = 12 / 4;
+    document.getElementById("division_result").innerHTML = "12 divided by 4 = " + Division;
+}
+function multiple_Operations() {
+    var Multiple = (3 +10) * 2 / 7;
+    document.getElementById("multiple_Numbers").innerHTML ="3 plus 10 times 2 divided by 7 = " + multiple_Operations;
+}
