91b61a1a2ba8df7ef379889be2b7c2e622fd90c7
gwebb17 <gwebb1723@gmail.com>
2022-06-30 09:09:32 -0700

    basic java function practice

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+var A = ("Jim said: " + "\"I am going to the market.\"");
+document.write(A);//Tells program to print var A
+
+var Birds = ("parakeet, owl"), C = ("New Jersey");
+document.write(C);//Will display string C
+
+document.write(6-3);//Will perform math operation and display result
+
