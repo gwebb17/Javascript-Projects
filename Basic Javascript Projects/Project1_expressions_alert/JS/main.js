f633eabfdcd2970e81b6cce2da7f02f5dd330d13
gwebb17 <gwebb1723@gmail.com>
2022-06-30 09:12:57 -0700

    more practice

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,4 @@
+window.alert("Hi");
 var A = ("Jim said: " + "\"I am going to the market.\"");
 document.write(A);//Tells program to print var A
 
@@ -5,4 +6,8 @@ var Birds = ("parakeet, owl"), C = ("New Jersey");
 document.write(C);//Will display string C
 
 document.write(6-3);//Will perform math operation and display result
+
+var Sent1=("This is the beginning of the string");
+var Sent2=(" and this is the end of the string");
+document.write(Sent1 + Sent2);//Prints combined variables
 
