77a44c0e36ea0937cfce83b3e83b1f084d12ac11
gwebb17 <gwebb1723@gmail.com>
2022-07-01 14:51:00 -0700

    Boolean additions1

---
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,54 @@
+function type_Function() {
+    var X="monkey";
+    document.write(typeof "monkey");
+}
+function type_Coercion() {
+    document.write("30" + 1.47);
+}
+function not_anumber() {
+    document.getElementById("nan").innerHTML=0/0;
+}
+function say_true() {
+    document.getElementById("true").innerHTML=isNaN('string');
+}
+function say_false() {
+    document.getElementById("false").innerHTML=isNaN(5);
+}
+function infinity() {
+    document.getElementById("infinite").innerHTML=2E310;
+}
+function neginfinity() {
+    document.getElementById("neginfinite").innerHTML=(-1.8076931348623157E308);
+}
+function boo1() {
+    document.getElementById("bool1").innerHTML=(17>=3);
+}
+function boo2() {
+    document.getElementById("bool2").innerHTML=(17>18);
+}
+function equals2() {
+    document.getElementById("equal2").innerHTML=(10+7)==17;
+}
+function equals1() {
+    document.getElementById("equal1").innerHTML=(10+7)==7;
+}
+function eqa() {
+    var eqa1=10;
+    var eqa1b=10;
+    document.getElementById("eqa").innerHTML=(eqa1===eqa1b);
+}
+function eqb() {
+    var eqa2=10;
+    var eqa2b="number";
+    document.getElementById("eqa2").innerHTML=(eqa2===eqa2b);
+}
+function eqc() {
+    var eqa3=10;
+    var eqa3b="10";
+    document.getElementById("eqa3").innerHTML=(eqa3===eqa3b);
+}
+function eqd() {
+    var eqa4=10;
+    var eqa4b=1;
+    document.getElementById("eqa4").innerHTML=(eqa4===eqa4b);
+}
