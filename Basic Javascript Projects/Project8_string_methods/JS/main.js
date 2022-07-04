2c05c38bfb8cd15a799346ba406324b74aacf0bb
gwebb17 <gwebb1723@gmail.com>
2022-07-04 10:22:31 -0700

    various methodology

--- Basic Javascript Projects/Project8_string_methods/JS/main.js
+++ Basic Javascript Projects/Project8_string_methods/JS/main.js
@@ -13,3 +13,41 @@ function slice_Function() {
     var dvadst=adeen.slice(13,16);
     document.getElementById("slice").innerHTML=dvadst;
 }
+
+//Uppercase//
+function up_Function() {
+    var lower="hello world";
+    var upper=lower.toUpperCase();
+    document.getElementById("upper").innerHTML=upper;
+}
+
+//Search//
+function search_Function() {
+    var look="My purple house";
+    var search=look.search("purple");
+    document.getElementById("search").innerHTML=search;
+}
+
+//toString//
+function string_Function() {
+    var thing=4000000;
+    document.getElementById("stringy").innerHTML=thing.toString();
+}
+
+//Precision//
+function precise_Function() {
+    var bignumber=75453.8765;
+    document.getElementById("precise").innerHTML=bignumber.toPrecision(7);
+}
+
+//Tofixed//
+function fix_Function() {
+    var fixnumber=17.23;
+    document.getElementById("fixed").innerHTML=fixnumber.toFixed(1);
+}
+
+//Valueof//
+function value_Function() {
+    var value="What type of data am i";
+    document.getElementById("value").innerHTML=value.valueOf();
+}
