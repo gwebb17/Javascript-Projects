b42d3cf2c696680564e998214e91181b83d7d81d
gwebb17 <gwebb1723@gmail.com>
2022-07-04 11:15:39 -0700

    slideshow assignment

---
+++ Basic Javascript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,47 @@
+//countdown function//
+function countdown() {
+    var seconds=document.getElementById("seconds").value;
+
+    function tick() {
+        seconds=seconds-1;
+        timer.innerHTML=seconds;
+        var time=setTimeout(tick, 1000);//pausing for 1000 milloseconds/1sec//
+        if (seconds ==-1) {
+            alert("Times up!");
+            clearTimeout(time);
+            timer.innerHTML="";
+        }
+
+    }
+    tick();
+}
+
+//Slideshow//
+let slideIndex=1;
+showSlides(slideIndex);
+
+//next/previous controls
+function plusSlides(n){
+    showSlides(slideIndex+=n);
+}
+
+//Thumbnail image controls//
+function currentSlide(n) {
+    showSlides(slideIndex=n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides=document.getElementsByClassName("mySlides");
+    let dots=document.getElementsByClassName("dot");
+    if (n>slides.length) {slideIndex=1}
+    if (n<1) {slideIndex=slides.length}
+    for (i=0; i<slides.length; i++) {
+        slides[i].style.display="none";
+    }
+    for (i=0; i<dots.length; i++) {
+        dots[i].className=dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display="block";
+    dots[slideIndex-1].className+=" active";
+}
